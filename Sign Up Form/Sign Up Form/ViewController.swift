5f1ba4c3b8d5a4d3dcf4acc0db89d35ea106ca98
shoaib <shoaib.sk.98@achiever-79.local>
2018-01-22 14:23:22 +0530

    assigned project

---
+++ Sign Up Form/Sign Up Form/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sign Up Form
+//
+//  Created by shoaib on 1/22/18.
+//  Copyright © 2018 shoaib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
